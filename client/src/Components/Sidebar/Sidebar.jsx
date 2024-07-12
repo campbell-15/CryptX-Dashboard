a8a9ddcf39a7746cd04f11a785b8d77e9cb31de9
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-07-12 17:51:26 +0200

    edited sidebar styling and content

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import './Sidebar.css';
+    import React from 'react';
+    import './Sidebar.css';
 
-const Sidebar = () => {
+    const Sidebar = () => {
     return (
         <div className="sidebar">
-        <div className="sidebar__logo">
-            <img src="logo.png" alt="CryptX Logo" />
+        <div className="logo">
+            <h1>CryptX</h1>
         </div>
-        <nav className="sidebar__nav">
+        <nav className="menu">
             <ul>
-            <li className="sidebar__item active">
-                <i className="icon-overview"></i>
+            <li className="menu-item active">
+                <i className="icon overview-icon"></i>
                 <span>Overview</span>
             </li>
-            <li className="sidebar__item">
-                <i className="icon-chart"></i>
+            <li className="menu-item">
+                <i className="icon chart-icon"></i>
                 <span>Chart</span>
             </li>
-            <li className="sidebar__item">
-                <i className="icon-transactions"></i>
+            <li className="menu-item">
+                <i className="icon transactions-icon"></i>
                 <span>Transactions</span>
             </li>
-            <li className="sidebar__item">
-                <i className="icon-wallet"></i>
+            <li className="menu-item">
+                <i className="icon wallet-icon"></i>
                 <span>Wallet</span>
             </li>
-            <li className="sidebar__item">
-                <i className="icon-mailbox"></i>
+            <li className="menu-item">
+                <i className="icon mailbox-icon"></i>
                 <span>Mail Box</span>
             </li>
-            <li className="sidebar__item">
-                <i className="icon-settings"></i>
+            <li className="menu-item">
+                <i className="icon settings-icon"></i>
                 <span>Setting</span>
             </li>
-            <li className="sidebar__item">
-                <i className="icon-logout"></i>
+            <li className="menu-item">
+                <i className="icon logout-icon"></i>
                 <span>Logout</span>
             </li>
             </ul>
         </nav>
         </div>
     );
-};
+    };
 
-export default Sidebar;
+    export default Sidebar;
