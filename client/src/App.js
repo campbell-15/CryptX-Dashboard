c05baaacf81cd0227e2cf0139dd226bc988feb65
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-07-12 17:05:13 +0200

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignUp,
+} from "@clerk/clerk-react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
@@ -11,15 +19,36 @@ const App = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div className="flex-1 flex flex-col">
-        <Header toggleSidebar={toggleSidebar} />
-        <div className="flex-1 overflow-auto">
-          <MainContent />
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route
+          path="/*"
+          element={
+            <SignedIn>
+              <div className="flex h-screen">
+                <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+                <div className="flex-1 flex flex-col">
+                  <Header toggleSidebar={toggleSidebar} />
+                  <div className="flex-1 overflow-auto">
+                    <MainContent />
+                  </div>
+                </div>
+              </div>
+            </SignedIn>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
+
+const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
