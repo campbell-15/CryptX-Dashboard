2d4bd40aed89842fdf20cc4e70554eb2be427ddf
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-07-14 21:23:39 +0200

    Header line added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,35 +2,38 @@ import React from "react";
 
 const Header = ({ toggleSidebar }) => {
   return (
-    <div className="flex justify-between items-center p-4 bg-white shadow-md">
-      <div className="text-gray-400 flex items-center w-full lg:w-auto">
-        <input
-          type="text"
-          placeholder="Search type of keywords"
-          className="p-2 bg-gray-100 rounded w-full lg:w-auto"
-        />
-      </div>
-      <div className="flex items-center">
-        <button
-          className="lg:hidden w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center"
-          onClick={toggleSidebar}
-        >
-          <i className="icon-menu"></i>
-        </button>
-        <div className="hidden lg:flex items-center">
-          <div className="w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center">
-            <i className="icon-bell"></i>
-          </div>
-          <div className="w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center">
-            <i className="icon-question"></i>
-          </div>
+    <>
+      <div className="flex justify-between items-center p-5 ">
+        <div className="text-gray-400 flex items-center w-full lg:w-auto">
+          <input
+            type="text"
+            placeholder="Search type of keywords"
+            className="p-2 bg-gray-100 rounded w-full lg:w-auto"
+          />
         </div>
         <div className="flex items-center">
-          <div className="w-8 h-8 bg-gray-300 rounded-full mr-2"></div>
-          <div className="text-gray-600">Laurice</div>
+          <button
+            className="lg:hidden w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center"
+            onClick={toggleSidebar}
+          >
+            <i className="icon-menu"></i>
+          </button>
+          <div className="hidden lg:flex items-center">
+            <div className="w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center">
+              <i className="icon-bell"></i>
+            </div>
+            <div className="w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center">
+              <i className="icon-question"></i>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="w-8 h-8 bg-gray-300 rounded-full mr-2"></div>
+            <div className="text-gray-600">Laurice</div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="border-b-2" style={{ margin: "0 1cm", width: "calc(100% - 2cm)" }}></div>
+    </>
   );
 };
 
