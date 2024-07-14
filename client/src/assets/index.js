bb6520be3b1a9931a2b70ca0dbf04c57313e48c8
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-07-14 22:13:06 +0200

    Added logo for header

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -5,6 +5,19 @@ import Settings from "./setting-2.svg";
 import Sms from "./sms.svg";
 import Wallet from "./wallet-2.svg";
 import Wallet2 from "./wallet-minus.svg";
-import Graph from "./graph.svg"
+import Graph from "./graph.svg";
+import Question from "./CryptX Dashboard help.svg";
+import Bell from "./notification.svg";
 
-export { Logo, Sqaure, Logout, Settings, Sms, Wallet, Wallet2, Graph };
+export {
+  Logo,
+  Sqaure,
+  Logout,
+  Settings,
+  Sms,
+  Wallet,
+  Wallet2,
+  Graph,
+  Question,
+  Bell,
+};
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Question, Bell } from "../assets";
 
 const Header = ({ toggleSidebar }) => {
   return (
     <>
-      <div className="flex justify-between items-center p-5 ">
-        <div className="text-gray-400 flex items-center w-full lg:w-auto">
+      <div className="flex justify-between items-center p-5 bg-white">
+        <div className="flex items-center w-full lg:w-auto">
           <input
             type="text"
             placeholder="Search type of keywords"
-            className="p-2 bg-gray-100 rounded w-full lg:w-auto"
+            className="p-2 bg-gray-100 rounded-full w-full lg:w-auto text-gray-600"
           />
         </div>
         <div className="flex items-center">
@@ -19,20 +20,23 @@ const Header = ({ toggleSidebar }) => {
             <i className="icon-menu"></i>
           </button>
           <div className="hidden lg:flex items-center">
-            <div className="w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center">
-              <i className="icon-bell"></i>
+            <div className="w-8 h-8 mr-4 flex items-center justify-center">
+              {" "}
+              <img src={Bell} alt="Overview Icon" />
             </div>
-            <div className="w-8 h-8 bg-gray-200 rounded-full mr-4 flex items-center justify-center">
-              <i className="icon-question"></i>
+            <div className="w-8 h-8 mr-4 flex items-center justify-center">
+              {" "}
+              <img src={Question} alt="Overview Icon" />
             </div>
           </div>
           <div className="flex items-center">
             <div className="w-8 h-8 bg-gray-300 rounded-full mr-2"></div>
-            <div className="text-gray-600">Laurice</div>
+            <div className="text-gray-600 font-semibold mr-1">Laurice</div>
+            <div className="text-gray-400">@laurice22</div>
           </div>
         </div>
       </div>
-      <div className="border-b-2" style={{ margin: "0 1cm", width: "calc(100% - 2cm)" }}></div>
+      <div className="border-b-2 border-gray-200 mx-7"></div>
     </>
   );
 };
